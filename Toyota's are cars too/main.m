c4e2e75bda159a9874374361ffdee3ce424a36e7
Auston Salvana <auston.salvana@live.com>
2015-06-18 18:09:53 -0700

    Initial Commit

---
+++ Toyota's are cars too/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Toyota's are cars too
+//
+//  Created by Auston Salvana on 6/18/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
